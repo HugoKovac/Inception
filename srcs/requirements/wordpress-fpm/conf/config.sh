a053e721af5329fc68017cdf21b37d83ae2c792d
hugokovac <kovmaxime@gmail.com>
2022-08-23 10:33:42 +0200

    missing wp conf

---
+++ srcs/requirements/wordpress-fpm/conf/config.sh
@@ -0,0 +1,9 @@
+#install wp-cli
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv -f wp-cli.phar /usr/local/bin/wp
+
+wp --
+
+#run php-fpm not daemonized
+php-fpm7.3 -F
---
+++ srcs/requirements/wordpress-fpm/conf/wp-config.php
@@ -0,0 +1,5 @@
+<?php
+	define("DB_NAME",				getenv("MARIADB_DATABASE"));
+	define("DB_USER",				getenv("MARIADB_USER"));
+	define("DB_PASSWORD",			getenv("MARIADB_PASSWORD"));
+	define("DB_HOST",				getenv("WORDPRESS_MARIADB_HOST") . ":3306");
